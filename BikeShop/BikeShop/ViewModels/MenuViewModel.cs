8b6c3a2288a0662a497cf15c66f767a650a5e45c
한경일 <hankil@hotmail.com>
2021-12-07 20:45:55 +0900

    1

---
+++ BikeShop/BikeShop/ViewModels/MenuViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BikeShop.ViewModels
+{
+    internal class MenuViewModel : Notifier
+    #region Logic
+
+    #endregion
+
+    #region
+    {
+
+
+        string _TextBox_Text;
+        public string TextBox_Text
+        {
+            get
+            {
+                return _TextBox_Text;   
+            }
+            set
+            {
+                _TextBox_Text=value;
+                OnPropertyChanged("TextBox_Text");
+              
+
+            }
+        }
+        private ICommand _leftButtonDownCommand;
+
+        public ICommand LeftMouseButtonDown
+        {
+            get
+            {
+                return _leftButtonDownCommand ?? (_leftButtonDownCommand = new RelayCommand(
+                   x =>
+                   {
+                       DoStuff();
+                   }));
+            }
+        }
+
+       
+
+        private static void DoStuff()
+        {
+            MessageBox.Show("Responding to left mouse button click event...");
+        }
+
+    }
+    #endregion
+}
