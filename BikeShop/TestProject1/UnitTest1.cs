c80fba852569a59c267ad85323f43863206c5bae
한경일 <hankil@hotmail.com>
2021-12-22 11:03:11 +0900

    UnitTest 추가

---
+++ BikeShop/TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        //[Repeat(10)]
+        public void Test1()
+        {
+            //Assert.Equal("Test", Method());
+        }
+    }
+}
